532b3da7d8338ec7abfd94ef4f065e9b3dd82a40
Tomasz Guz <t.guz@micro-solutions.pl>
2022-04-13 21:46:05 +0200

    1 lekcja

--- Sumy.py
+++ Sumy.py
@@ -1,10 +1,19 @@
-x = input('Podaj pierwsza liczbe w ciagu od 1 do 100').split()
-y = input('Podaj ostatnia liczbe w ciagu od 1 do 100').split()
+
+
+x = int (input('Podaj pierwsza liczbe w ciagu od 1 do 100: '))
+y = int (input('Podaj ostatnia liczbe w ciagu od 1 do 100: '))
+
 
 i = 0
 print ('Kolejne sumy liczb w ciagu ', x, y)
-for n in range (y-x):
+for n in range (x, y+1):
     i = i + n
     print (i)
 
 
+n = 0
+i = x
+while i <= y: 
+    n = n + i
+    print (n)
+    i+=1
